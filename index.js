b70aa3409a5f84b023111fa6c072b60111eb5fd0
Aviad Sapir <aviadsapir@gmail.com>
2018-09-09 21:51:02 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout(string){
+  return string.toUpperCase()
+}
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logWhisper(string){
+  console.log(whisper(string))
+}
+function logShout(string){
+  console.log(shout(string))
+}
+function sayHiToGrandma
